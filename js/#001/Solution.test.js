f591bdf1b85a9816c5b564820ea47ab48bccade7
indiansage <indiansage.work@gmail.com>
2020-09-10 10:49:50 +0530

    JS test setup and added a test

--- js/#001/Solution.test.js
+++ js/#001/Solution.test.js
@@ -1,8 +1,10 @@
+import isSolutionPossible from './Solution';
+
 describe('Test Solution #001', () => {
-    test('should return true', () => {
-        
-    })
-    test('should return false', () => {
-        
-    })
-})
+	test('should return true', () => {
+		expect(isSolutionPossible([4, 8, -2, 0, 6], 2)).toBe(true);
+	});
+	test('should return false', () => {
+		expect(isSolutionPossible([4, 8, -1, 0, 6], 2)).toBe(false);
+	});
+});
