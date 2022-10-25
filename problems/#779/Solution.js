7e07e66078a639dd7e51f2c98aa9985341ad8e3d
indiansage <indiansage.work@gmail.com>
2022-10-25 23:34:54 +0530

    Added 780 question

--- problems/#779/Solution.js
+++ problems/#779/Solution.js
@@ -1,10 +1,10 @@
 /**
  * Find the minimum number of trial drops, in the worst-case, to find threshold floor where egg breaks.
- * @param {number} number number of floors to check
  * @param {number} number number of eggs in hand
+ * @param {number} number number of floors to check
  * @return {number} number
  */
-const findMinimumTrialDrops = (floors, eggs) => {
+const findMinimumTrialDrops = (eggs, floors) => {
 	if (eggs === 1) return floors; // Base case - if only one egg in hand, we have to try at each floor.
 };
 
--- problems/#779/Solution.test.js
+++ problems/#779/Solution.test.js
@@ -2,6 +2,9 @@ import findMinimumTrialDrops from "./Solution";
 
 describe("Test Solution #779", () => {
 	test("should return correct solution", () => {
-		expect(findMinimumTrialDrops(5, 1)).toBe(5);
+		// expect(findMinimumTrialDrops(eggs, floors)).toBe(result);
+		expect(findMinimumTrialDrops(1, 5)).toBe(5);
+		expect(findMinimumTrialDrops(2, 10)).toBe(4);
+		expect(findMinimumTrialDrops(5, 36)).toBe(8);
 	});
 });
